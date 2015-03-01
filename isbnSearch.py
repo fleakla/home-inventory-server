bed1dcd946d68cc82a2ca18563df3cd35c8631e0
fleak <fleakla@gmail.com>
2015-03-01 01:28:07 -0600

    a working search of google books by ISBN and template that displays search results

--- isbnSearch.py
+++ isbnSearch.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 
 from bottle import Bottle, template, request
 from google.appengine.api import urlfetch
@@ -9,15 +10,47 @@ isbn_search_param_str = "{ISBN:%s}"
 
 isbnBottle = Bottle()
 
+wanted_gbooks_fields = ['selfLink', 'volumeInfo']
+wanted_volume_info = ['title', 'subtitle', 'authors', 'publisher', 'publishedDate', 'description',
+                      'industryIdentifiers',
+                      'pageCount', 'categories', 'imageLinks', 'previewLink', 'canonicalVolumeLink']
+
+
+def extract_fields(gbooks_results=None):
+    ret_hash = {}
+    if gbooks_results and gbooks_results['totalItems'] > 0:
+        primary_result = gbooks_results['items'][0]
+        for field in wanted_gbooks_fields:
+            # print(field)
+            if field == 'volumeInfo':
+                for info_item in wanted_volume_info:
+                    if info_item in primary_result[field]:
+                        # print("     " + info_item)
+                        ret_hash[info_item] = primary_result[field][info_item]
+                    else:
+                        # print("      no " + info_item)
+                        ret_hash[info_item] = None
+            else:
+                if field in primary_result:
+                    # print("  found " + field)
+                    ret_hash[field] = primary_result[field]
+                else:
+                    # print("  not found " + field)
+                    ret_hash[field] = None
+
+    return ret_hash
+
 
 @isbnBottle.route('/searchISBN/', method=['GET'])
 def searchISBN():
     isbn_str = request.GET.get('ISBN')
-    results = None
+    extracted = None
     if isbn_str:
         parm = isbn_search_param_str % isbn_str
         url_str = google_books_url % parm
         results = urlfetch.fetch(url=url_str)
-        print(results.content)
+        # print(results.content)
+        results = json.loads(results.content)
+        extracted = extract_fields(results)
 
-    return template('webtemplates/inventory/searchISBN')
+    return template('webtemplates/inventory/searchISBN', srch_results=extracted)
--- webtemplates/inventory/searchISBN.tpl
+++ webtemplates/inventory/searchISBN.tpl
@@ -7,9 +7,26 @@
                 ISBN: <input type="text" name="ISBN"/>
                 <input type="submit" value="Search" name="isbnSearchButton"/>
             </form>
-        </div>
+        </div><br/>
 
-
+        % if srch_results:
+            <div style="border: 1px solid black;">
+                {{srch_results}}
+            </div>
+            <br/>
+            <div style="border: 1px solid black;">
+                <p>Title: {{srch_results['title'] + (' - ' + srch_results['subtitle'] if srch_results['subtitle'] else '')}} <br/></p>
+                % if srch_results['imageLinks']:
+                    <img src="{{srch_results['imageLinks']['smallThumbnail']}}"/>
+                    <br/>
+                % end
+                <p>Description: {{srch_results['description'] if 'description' in srch_results else 'no description'}}</p>
+            </div>
+        % else:
+            <div>
+                <h2>No results to display!</h2>
+            </div>
+        % end
     </body>
 
 </html>
